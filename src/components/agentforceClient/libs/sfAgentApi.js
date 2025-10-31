e7290fbf5c31b49ff4e041a27cbe1241ee1c0de1
trevSmart <203040921+trevSmart@users.noreply.github.com>
2025-10-31 01:42:51 +0100

    dssdsd

--- src/components/agentforceClient/libs/sfAgentApi.js
+++ src/components/agentforceClient/libs/sfAgentApi.js
@@ -119,7 +119,7 @@ export default class SfAgentApi extends EventTarget {
 
 		const data = await response.json();
 
-		setStoredAccessToken(data.access_token);
+		sessionStorage.setItem('nextBankSalesforceAccessToken', data.access_token);
 		salesforceParameters.accessToken = data.access_token;
 		return response;
 	}
